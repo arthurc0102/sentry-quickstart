57141f2ebd56c108a1f1415d9af77232b4b926e2
Arthur Chang <arthurc0102@gmail.com>
2020-02-07 15:10:28 +0800

    feat(home): log environment on load

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +12,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(environment);
   }
 
   go() {
